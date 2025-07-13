fb34cc23c0795558f9c767f3d1c35e59138a0349
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-13 20:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
-
-  const subtractiveMap = [
+  const romanMap = [
+    ['M', 1000],
     ['CM', 900],
+    ['D', 500],
     ['CD', 400],
+    ['C', 100],
     ['XC', 90],
+    ['L', 50],
     ['XL', 40],
+    ['X', 10],
     ['IX', 9],
-    ['IV', 4]
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
   ];
 
-  let result = "";
+  let result = '';
 
-  for (let [symbol, value] of subtractiveMap) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
-    }
-  }
-
-  for (let i = 0; i <= 6; i++) {
-    const [symbol, value] = obj[i];
+  for (const [symbol, value] of romanMap) {
     while (num >= value) {
       result += symbol;
       num -= value;
@@ -38,5 +27,4 @@ function convertToRoman(num) {
   return result;
 }
 
-// ✅ Make sure this is at the bottom:
 module.exports = convertToRoman;
